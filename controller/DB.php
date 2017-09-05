01a29ba2b05fc7547edb612ba258300e36dfef66
Rodrigo Mendes <rodrigomendes@atratis.com.br>
2017-09-05 14:54:13 -0300

    Add charset conexao DB

--- controller/DB.php
+++ controller/DB.php
@@ -8,7 +8,7 @@ class DB
     {
         if (!isset(self::$instance)) {
             try {
-                self::$instance = new PDO('mysql:host=localhost;dbname=just', "root", "1234");
+                self::$instance = new PDO('mysql:host=localhost;dbname=just;charset=utf8', "root", "1234");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (PDOException $e) {
